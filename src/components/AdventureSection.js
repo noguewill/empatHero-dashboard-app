cdbfedab02e5f0c0f6bb0a74486c346b4c6e5af5
noguewill <nogue.will@gmail.com>
2021-07-14 07:14:15 -0300

    Grid overflow system on students table set

--- src/components/AdventureSection.js
+++ src/components/AdventureSection.js
@@ -6,23 +6,47 @@ const AdventureSection = () => {
         <div className="bg-yellow-400 w-full h-4/5 flex justify-between items-center">
           {/* Inner container for the Adventure Theme Card*/}
           <div className="bg-blue-400 w-1/4 h-4/5 rounded-md flex">
-            <div className="bg-purple-500 w-10/12 h-5/6"></div>
+            <div className="bg-purple-500 w-11/12 h-5/6 flex flex-col justify-around py-5">
+              <div>
+                <h1 className="2xl:text-xl font-bold">Empa Safari</h1>
+                <h2 className="2xl:text-lg">
+                  Un'avventura incredibile, piena di nuovi amici!
+                </h2>
+              </div>
+              <h1 className="underline">SCOPRI DI PIÚ</h1>
+            </div>
             {/* Aventure theme card checkbox container */}
-            <div className="bg-green-500 w-2/12 h-full flex flex-col items-center pt-5">
+            <div className="bg-green-500 w-1/12 h-full flex flex-col items-center pt-5">
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
             </div>
           </div>
           <div className="bg-blue-400 w-1/4 h-4/5 rounded-md flex">
-            <div className="bg-purple-500 w-10/12 h-5/6"></div>
+            <div className="bg-purple-500 w-11/12 h-5/6 flex flex-col justify-around py-5">
+              <div>
+                <h1 className="2xl:text-xl font-bold">Viaggio nel Tempo</h1>
+                <h2 className="2xl:text-lg">
+                  Un'avventura incredibile, piena di nuovi amici!
+                </h2>
+              </div>
+              <h1 className="underline">SCOPRI DI PIÚ</h1>
+            </div>
             {/* Aventure theme card checkbox container */}
-            <div className="bg-green-500 w-2/12 h-full flex flex-col items-center pt-5">
+            <div className="bg-green-500 w-1/12 h-full flex flex-col items-center pt-5">
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
             </div>
           </div>
-          <div className="bg-blue-400 w-1/4 h-4/5 rounded-md flex">
-            <div className="bg-purple-500 w-10/12 h-5/6"></div>
+          <div className="bg-blue-400 w-1/4 h-4/5 rounded-md flex ">
+            <div className="bg-purple-500 w-11/12 h-5/6 flex flex-col justify-around py-5  ">
+              <div>
+                <h1 className="2xl:text-xl font-bold">Empa Spazio</h1>
+                <h2 className="2xl:text-lg">
+                  Un'avventura incredibile, piena di nuovi amici!
+                </h2>
+              </div>
+              <h1 className="underline">SCOPRI DI PIÚ</h1>
+            </div>
             {/* Aventure theme card checkbox container */}
-            <div className="bg-green-500 w-2/12 h-full flex flex-col items-center pt-5">
+            <div className="bg-green-500 w-1/12 h-full flex flex-col items-center pt-5">
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
             </div>
           </div>
--- src/components/StudentsTable.js
+++ src/components/StudentsTable.js
@@ -2,11 +2,11 @@ const StudentsTable = () => {
   return (
     <>
       {/* Classes container */}
-      <section className="bg-blue-500 w-11/12 h-2/5 flex justify-center items-center px-5">
+      <section className="bg-blue-500 w-11/12 h-2/5 flex justify-center items-center  px-5">
         {/* TABLE */}
-        <div className="bg-yellow-400 w-full h-6/7">
+        <div className="w-full h-6/7 ">
           {/* Table category title container */}
-          <div className="bg-green-500 w-full h-1/6 grid grid-cols-6 place-items-center font-bold ">
+          <div className="bg-green-500 w-full h-1/6 grid grid-cols-6 place-items-center justify-center font-bold 2xl:text-xl ">
             <h4>Alunno</h4>
             <h4>Soprannome</h4>
             <h4>Bag</h4>
@@ -15,9 +15,15 @@ const StudentsTable = () => {
             <h4>Disegno mostrato</h4>
           </div>
           {/* Table inner container */}
-          <div className="bg-pink-500 w-full h-6/7 grid grid-cols-6  place-items-center ">
+          <div className="bg-pink-500 w-full h-6/7 grid grid-cols-6  place-items-center overflow-y-scroll">
             {/* Table column elements */}
-            <div className="bg-blue-400 w-10/12 h-full flex flex-col items-center justify-around 2xl:text-xl">
+            <div className="bg-blue-400 w-10/12 h-full grid grid-cols-1 gap-4 place-items-center 2xl:text-xl">
+              <h4>first name</h4>
+              <h4>first name</h4>
+              <h4>first name</h4>
+              <h4>first name</h4>
+              <h4>first name</h4>
+              <h4>first name</h4>
               <h4>first name</h4>
               <h4>first name</h4>
               <h4>first name</h4>
@@ -25,7 +31,13 @@ const StudentsTable = () => {
               <h4>first name</h4>
               <h4>first name</h4>
             </div>
-            <div className="bg-blue-400 w-10/12 h-full flex flex-col items-center justify-around 2xl:text-xl">
+            <div className="bg-blue-400 w-10/12 h-full grid grid-cols-1 gap-4 place-items-center 2xl:text-xl">
+              <h4>surname</h4>
+              <h4>surname</h4>
+              <h4>surname</h4>
+              <h4>surname</h4>
+              <h4>surname</h4>
+              <h4>surname</h4>
               <h4>surname</h4>
               <h4>surname</h4>
               <h4>surname</h4>
@@ -33,7 +45,13 @@ const StudentsTable = () => {
               <h4>surname</h4>
               <h4>surname</h4>
             </div>
-            <div className="bg-blue-400 w-2/12 h-full flex flex-col items-center justify-around 2xl:text-xl">
+            <div className="bg-blue-400 w-2/12 h-full grid grid-cols-1 gap-4 place-items-center 2xl:text-xl">
+              <h4>1</h4>
+              <h4>2</h4>
+              <h4>3</h4>
+              <h4>4</h4>
+              <h4>5</h4>
+              <h4>6</h4>
               <h4>1</h4>
               <h4>2</h4>
               <h4>3</h4>
@@ -41,7 +59,13 @@ const StudentsTable = () => {
               <h4>5</h4>
               <h4>6</h4>
             </div>
-            <div className="bg-blue-400 w-2/12 h-full flex flex-col justify-around items-center">
+            <div className="bg-blue-400 w-2/12 h-full grid grid-cols-1 gap-4 place-items-center">
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
@@ -49,7 +73,13 @@ const StudentsTable = () => {
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
             </div>
-            <div className="bg-blue-400 w-2/12 h-full flex flex-col justify-around items-center">
+            <div className="bg-blue-400 w-2/12 h-full grid grid-cols-1 gap-4 place-items-center">
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
@@ -57,7 +87,13 @@ const StudentsTable = () => {
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
             </div>
-            <div className="bg-blue-400 w-2/12 h-full flex flex-col justify-around items-center">
+            <div className="bg-blue-400 w-2/12 h-full grid grid-cols-1 gap-4 place-items-center">
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
               <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
