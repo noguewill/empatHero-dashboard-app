489411a99597ad6fa9bbd6dede8ac8ca51bcd39b
noguewill <nogue.will@gmail.com>
2021-07-14 05:02:10 -0300

    inner elements of table columns set

--- src/components/EventContainer.js
+++ src/components/EventContainer.js
@@ -12,65 +12,65 @@ const EventContainer = () => {
       <div className="bg-blue-500 w-11/12 h-2/5 flex justify-center items-center px-5">
         {/* TABLE */}
         <div className="bg-yellow-400 w-full h-6/7">
-          {/* Table category container */}
-          <div className="bg-green-500 w-full h-1/6">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+          {/* Table category title container */}
+          <div className="bg-green-500 w-full h-1/6 grid grid-cols-6 place-items-center font-bold ">
+            <h4>Alunno</h4>
+            <h4>Soprannome</h4>
+            <h4>Bag</h4>
+            <h4>Carica disegno</h4>
+            <h4>Visualizza disegno</h4>
+            <h4>Disegno mostrato</h4>
           </div>
           {/* Table inner container */}
           <div className="bg-pink-500 w-full h-6/7 grid grid-cols-6  place-items-center ">
             {/* Table column elements */}
-            <div className="bg-blue-400 w-10/12 h-full ">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className="bg-blue-400 w-10/12 h-full flex flex-col items-center justify-around 2xl:text-xl">
+              <h4>first name</h4>
+              <h4>first name</h4>
+              <h4>first name</h4>
+              <h4>first name</h4>
+              <h4>first name</h4>
+              <h4>first name</h4>
             </div>
-            <div className="bg-blue-400 w-10/12 h-full">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className="bg-blue-400 w-10/12 h-full flex flex-col items-center justify-around 2xl:text-xl">
+              <h4>surname</h4>
+              <h4>surname</h4>
+              <h4>surname</h4>
+              <h4>surname</h4>
+              <h4>surname</h4>
+              <h4>surname</h4>
             </div>
-            <div className="bg-blue-400 w-2/12 h-full">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className="bg-blue-400 w-2/12 h-full flex flex-col items-center justify-around 2xl:text-xl">
+              <h4>1</h4>
+              <h4>2</h4>
+              <h4>3</h4>
+              <h4>4</h4>
+              <h4>5</h4>
+              <h4>6</h4>
             </div>
-            <div className="bg-blue-400 w-2/12 h-full">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className="bg-blue-400 w-2/12 h-full flex flex-col justify-around items-center">
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
             </div>
-            <div className="bg-blue-400 w-2/12 h-full">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className="bg-blue-400 w-2/12 h-full flex flex-col justify-around items-center">
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-full "></div>
             </div>
-            <div className="bg-blue-400 w-2/12 h-full">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className="bg-blue-400 w-2/12 h-full flex flex-col justify-around items-center">
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
             </div>
           </div>
         </div>
@@ -88,15 +88,21 @@ const EventContainer = () => {
           {/* Inner container for the Adventure Theme Card*/}
           <div className="bg-blue-400 w-1/4 h-4/5 rounded-md flex">
             <div className="bg-purple-500 w-10/12 h-5/6"></div>
-            <div className="bg-green-500 w-2/12 h-full"></div>
+            <div className="bg-green-500 w-2/12 h-full flex flex-col items-center pt-5">
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+            </div>
           </div>
           <div className="bg-blue-400 w-1/4 h-4/5 rounded-md flex">
             <div className="bg-purple-500 w-10/12 h-5/6"></div>
-            <div className="bg-green-500 w-2/12 h-full"></div>
+            <div className="bg-green-500 w-2/12 h-full flex flex-col items-center pt-5">
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+            </div>
           </div>
           <div className="bg-blue-400 w-1/4 h-4/5 rounded-md flex">
             <div className="bg-purple-500 w-10/12 h-5/6"></div>
-            <div className="bg-green-500 w-2/12 h-full"></div>
+            <div className="bg-green-500 w-2/12 h-full flex flex-col items-center pt-5">
+              <div className="bg-gray-500 2xl:w-7 2xl:h-7 rounded-md "></div>
+            </div>
           </div>
         </div>
         {/* Adventure Theme Button Confirmation */}
