4c29cba49d2345cb1eaf1f56a9444629f45ee148
noguewill <nogue.will@gmail.com>
2021-07-15 05:02:54 -0300

    Overflow table title head bug fix

--- src/components/StudentsTable.js
+++ src/components/StudentsTable.js
@@ -6,18 +6,21 @@ const StudentsTable = () => {
         {/* TABLE */}
         <div className="w-full h-full">
           {/* Table category title container */}
-          <div className="bg-green-500 w-full h-1/6 grid grid-cols-6 place-items-center overflow-y-scroll  font-bold 2xl:text-xl ">
-            <h4 className="2xl:text-base">Alunno</h4>
-            <h4 className="2xl:text-base">Soprannome</h4>
-            <h4 className="2xl:text-base">Bag</h4>
-            <h4 className="2xl:text-base">Carica disegno</h4>
-            <h4 className="2xl:text-base">Visualizza disegno</h4>
-            <h4 className="2xl:text-base">Disegno mostrato</h4>
+          <div className="w-full h-1/6 flex">
+            <div className="bg-green-500 w-full h-full grid grid-cols-6 place-items-center  font-bold 2xl:text-xl rounded-tl-xl">
+              <h4 className="2xl:text-base">Alunno</h4>
+              <h4 className="2xl:text-base">Soprannome</h4>
+              <h4 className="2xl:text-base">Bag</h4>
+              <h4 className="2xl:text-base">Carica disegno</h4>
+              <h4 className="2xl:text-base">Visualizza disegno</h4>
+              <h4 className="2xl:text-base">Disegno mostrato</h4>
+            </div>
+            <div className=" bg-green-500 w-6 h-full rounded-tr-xl"></div>
           </div>
           {/* Table inner container */}
           <div className="bg-pink-500 w-full h-6/7 grid grid-cols-6  place-items-center overflow-y-scroll">
             {/* Table column elements */}
-            <div className="bg-blue-400 w-10/12 h-full grid grid-cols-1 gap-4 place-items-center 2xl:text-xl">
+            <div className="bg-blue-400 w-10/12 h-full grid grid-cols-1 gap-4 place-items-center 2xl:text-xl rounded-b-xl">
               <h4>first name</h4>
               <h4>first name</h4>
               <h4>first name</h4>
