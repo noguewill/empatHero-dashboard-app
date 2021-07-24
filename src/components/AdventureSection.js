67295d72c76a0c7bd7591090c551f4198bfa6607
noguewill <nogue.will@gmail.com>
2021-07-24 10:17:30 -0300

    Prop theme creation

--- src/components/AdventureSection.js
+++ src/components/AdventureSection.js
@@ -5,11 +5,11 @@ const AdventureSection = () => {
       {/* Container of Adventure Section */}
       <section className="w-10/12 h-2/6  flex flex-col items-center">
         <div className=" w-full h-5/6 grid grid-cols-3 gap-4 place-items-center">
-          <AdventureCard />
-          <AdventureCard />
-          <AdventureCard />
+          {/*Props indicates the current theme*/}
+          <AdventureCard theme={0} />
+          <AdventureCard theme={1} />
+          <AdventureCard theme={2} />
         </div>
-
         {/* Adventure Theme Button Confirmation */}
         <button className="bg-green-300 2xl:w-1/5  2xl:h-12 lg:w-44 lg:h-9 rounded-lg  self-end  text-white">
           <p className="2xl:text-sm font-bold  lg:text-xxs ">
