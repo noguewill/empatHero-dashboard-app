bd1f99726d8e376de07d94bc3308f5f3541ce990
noguewill <nogue.will@gmail.com>
2021-07-28 09:41:16 -0300

    Setup states for confirmAdventure done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import EventContainer from "./components/EventContainer";
 import NavBar from "./components/Navbar";
 const App = () => {
   const [confirmAdventure, setConfirmAdventure] = useState(false);
+  const [currAdventure, setCurrAdventure] = useState();
+
+  console.log(currAdventure);
   return (
     <>
       {/* Placeholder Flexbox container positioning */}
+      {confirmAdventure ? (
+        <div className="absolute w-screen h-screen bg-gray-900 bg-opacity-40 flex justify-center items-center"></div>
+      ) : null}
       <NavBar />
       <div className="flex w-full h-6/7">
         <CalendarSideBar />
-        <EventContainer />
+        <EventContainer
+          currAdventure={currAdventure}
+          setCurrAdventure={setCurrAdventure}
+          setConfirmAdventure={setConfirmAdventure}
+        />
       </div>
     </>
   );
