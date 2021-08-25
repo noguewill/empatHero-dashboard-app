1a5401b08cb7936fd4e3f3a26523456860c45810
noguewill <nogue.will@gmail.com>
2021-08-25 20:16:12 -0300

    Proportion adjustment for 1200 screens

--- src/components/StudentsTable.js
+++ src/components/StudentsTable.js
@@ -9,12 +9,18 @@ const StudentsTable = ({ classChosen }) => {
         {/* Table category title container */}
         <div className="w-full 3xl:h-16 2xl:h-14 h-14 lg:h-10 flex">
           <div className="bg-empatLightBlue bg-opacity-30 w-full h-full z-10 3xl:text-xl 2xl:text-lg text-xs font-bold grid grid-cols-6 place-items-center rounded-tl-xl ">
-            <h4 className="pl-10 place-self-start self-center">Alunno</h4>
-            <h4 className="vhPadding 3xl:pl-2 2xl:pl-16 pl-14">Soprannome</h4>
-            <h4 className="2xl:pl-14 pl-14">Bag</h4>
-            <h4 className="2xl:pl-14 pl-14">Carica disegno</h4>
-            <h4 className="3xl:pl-14 2xl:pl-14 pl-9">Visualizza disegno</h4>
-            <h4 className="3xl:pl-12 pl-9">Disegno mostrato</h4>
+            <h4 className="xl:pl-10 place-self-start self-center pl-9">
+              Alunno
+            </h4>
+            <h4 className="vhPadding 3xl:pl-2 2xl:pl-16 xl:pl-14 pl-16">
+              Soprannome
+            </h4>
+            <h4 className="2xl:pl-14 xl:pl-14 pl-14">Bag</h4>
+            <h4 className="2xl:pl-14 xl:pl-14 pl-11">Carica disegno</h4>
+            <h4 className="3xl:pl-14 2xl:pl-14 xl:pl-9 pl-7">
+              Visualizza disegno
+            </h4>
+            <h4 className="3xl:pl-12 xl:pl-9 pl-8">Disegno mostrato</h4>
           </div>
           {/* !important  positional clutch to adapt to scroll table width*/}
           <div className=" bg-empatLightBlue bg-opacity-30 w-6 h-full rounded-tr-xl z-10"></div>
